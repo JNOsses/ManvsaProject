fa7b9a663dcde7c2ae18c62361a1a31c83cd829a
JNOsses <juan.nicolas.osses@gmail.com>
2021-06-21 00:30:09 -0300

    JS/Scroll - Css @medias

---
+++ scroll.js
@@ -0,0 +1,35 @@
+/*con esta variable llamo al boton*/
+buttonUp = document.getElementById("button-up");
+
+/*Scroll up*/
+function scrollUp(){
+    var currentScroll = document.documentElement.scrollTop;
+    if (currentScroll > 0) {
+    
+    window.scrollTo (0,0);
+    buttonUp.style.transform ="scale(0)"
+    }
+}
+
+document.getElementById("button-up").addEventListener("click",scrollUp);
+
+
+
+
+
+/**/ 
+
+window.onscroll = function (){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if(scroll > 810){
+        buttonUp.style.transform = "scale(1)";
+    } else if (scroll < 810 )
+    {
+        buttonUp.style.transform = "scale(0)";
+    }
+}
+
+
+
