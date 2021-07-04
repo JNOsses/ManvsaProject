7753076d8cd621304df4cb53919369e57663f383
JNOsses <juan.nicolas.osses@gmail.com>
2021-07-04 20:27:40 -0300

    Cambio performatico FRONT / Cambios Responsives

---
+++ Mostrar.js
@@ -0,0 +1,23 @@
+function mostrarSeccionDiscografia() {
+    document.getElementById("SeccionComfort").style.display = "inline-flex";
+
+
+    document.getElementById("SeccionSkydive").style.display = "inline-flex";
+
+    document.getElementById("SeccionMyself").style.display = "inline-flex";
+   
+    
+}   
+
+function mostrarSeccionBiografia(){
+    document.getElementById("SeccionBiografia").style.display = "block";
+}
+
+function mostrarSeccionVideo(){
+    document.getElementById("SeccionVideo").style.display = "inline-block";
+}
+
+
+function mostrarSeccionContacto(){
+    document.getElementById("SeccionContacto").style.display = "block";
+}
